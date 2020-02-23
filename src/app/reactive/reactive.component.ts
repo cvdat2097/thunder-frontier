80ceeb2b8886fcfad3e098d6520c3503c325938b
cvdat2097 <cvdat2097@gmail.com>
2020-02-23 19:23:17 +0700

    reactive form validation

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormControlName } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormControlName,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -10,12 +15,16 @@ export class ReactiveComponent {
   emailControl = new FormControl('default email');
   applicationGroup = new FormGroup({
     email: this.emailControl,
-    username: new FormControl('admin0012'),
+    username: new FormControl('admin0012', [Validators.required]),
     education: new FormGroup({
       highSchool: new FormControl('Le Hong Phong school for the gifted'),
       year: new FormControl(156)
     })
   });
+
+  constructor() {
+    window.reactiveForm = this.applicationGroup;
+  }
 
   reactiveFormSubmitHandler() {
     console.log('Submitted value: ', this.applicationGroup.value);
