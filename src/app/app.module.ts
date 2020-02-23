9ef7314bd34ace0ef93b6f04a764af8bee65351e
cvdat2097 <cvdat2097@gmail.com>
2020-02-23 19:40:07 +0700

    template form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [AppComponent, ReactiveComponent, TemplateComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html'
 })
 export class TemplateComponent {
-  // Reactive Forms
-  emailControl = new FormControl('default email');
-  applicationGroup = new FormGroup({
-    email: this.emailControl,
-    username: new FormControl('admin0012'),
-    education: new FormGroup({
-      highSchool: new FormControl('Le Hong Phong school for the gifted'),
-      year: new FormControl(156)
-    })
-  });
+  // Template driven Forms
+  application = {
+    email: 'admin@gmail.com',
+    username: 'admin',
+    education: {
+      highSchool: 'Le Hong Phong school for the gifted',
+      year: 2005
+    }
+  };
 
-  reactiveFormSubmitHandler() {
-    console.log('Submitted value: ', this.applicationGroup.value);
+  constructor() {
+    window.templateForm = this.application;
+  }
+
+  submitHandler() {
+    console.log('Submitted value: ', this.application);
   }
 }
